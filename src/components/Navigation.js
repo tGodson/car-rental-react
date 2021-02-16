38f03aaa5b2540b306e7ec977c1c74af929ad24d
tGodson <tendongze95@gmail.com>
2021-02-16 15:14:08 +0100

    add condition when token is null

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -16,7 +16,7 @@ export default function Navigation() {
         <Nav className="mr-auto" />
         <Nav>
           {
-            token === undefined || token === 'undefined'
+            token === undefined || token === 'undefined' || token === null
               ? (
                 <>
                   <Link className="nav-link white" to="/login">SIGN IN</Link>
